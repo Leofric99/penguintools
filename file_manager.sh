c3b59b944c422e4d081995f33407cec3b0f29f39
ContainerisedNomad <github@christopherhodges.net>
2024-07-16 21:25:31 +0100

    Added ls command option

--- file_manager.sh
+++ file_manager.sh
@@ -3,11 +3,14 @@
 # Function to display the menu
 display_menu() {
     echo "Menu:"
+    echo
     echo "1. Rename a file"
     echo "2. Move a file"
     echo "3. Copy a file"
     echo "4. Mirror a directory"
-    echo "5. Exit"
+    echo "5. Run the ls command on a directory"
+    echo "6. Exit"
+    echo
 }
 
 # Function to handle option 1: Rename a file
@@ -56,7 +59,15 @@ copy_file() {
 
 # Function to handle option 4: Mirror a directory
 mirror_directory() {
-    echo "Note: Large directories are recommended to be performed in a tmux session due to potential duration."
+    echo "Note: Large directories are recommended to be performed in a"
+    echo "      tmux session due to potential duration of operation."
+    echo
+    echo "      !! Use with Caution !!"
+    echo "      This will mirror the contents of a directory. Any files"
+    echo "      that are non-existant on the source dir will be removed"
+    echo "      from the destination dir."
+    echo "      !! Use with caution !!"
+    echo
     echo "Enter the source directory:"
     read -e SOURCE_DIR
 
@@ -68,18 +79,33 @@ mirror_directory() {
     echo "Operation completed."
 }
 
+# Function to handle option 5: List contents of a directory
+list_directory_contents() {
+    echo "Enter the directory to run ls on:"
+    read -e dir
+    read -p "Enter any arguments for the ls command (leave blank for none): " args
+    if [ -d "$dir" ]; then
+        echo "Output of ls ${args:-} $dir:"
+        echo
+        ls ${args:-} "$dir" || echo "Error: Failed to run ls on $dir."
+    else
+        echo "Error: Directory $dir does not exist."
+    fi
+}
+
 # Main loop
 while true; do
     display_menu
 
-    read -p "Enter your choice (1-5): " choice
+    read -p "Enter your choice (1-6): " choice
     case $choice in
         1) rename_file ;;
         2) move_file ;;
         3) copy_file ;;
         4) mirror_directory ;;
-        5) echo "Exiting program."; break ;;
-        *) echo "Invalid option. Please enter a number between 1 and 5." ;;
+        5) list_directory_contents ;;
+        6) echo "Exiting program."; break ;;
+        *) echo "Invalid option. Please enter a number between 1 and 6." ;;
     esac
 
     read -p "Operation completed. Do you want to return to the menu? (y/n): " answer
