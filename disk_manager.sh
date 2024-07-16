8e8e4341ab8e8648d9ad12cf9c2123398970739c
ContainerisedNomad <github@christopherhodges.net>
2024-07-16 19:48:50 +0100

    Added error checking

--- disk_manager.sh
+++ disk_manager.sh
@@ -21,18 +21,38 @@ while true; do
         1)
             echo "Output of lsblk:"
             echo
-            lsblk
+            lsblk || echo "Error: Failed to run lsblk."
             ;;
         2)
             read -p "Enter the drive/device path to mount: " drive_path
             read -p "Enter the directory to mount to: " mount_point
-            sudo mount $drive_path $mount_point
-            echo "Drive mounted successfully."
+            if [ -b "$drive_path" ]; then
+                if [ -d "$mount_point" ]; then
+                    sudo mount $drive_path $mount_point
+                    if [ $? -eq 0 ]; then
+                        echo "Drive mounted successfully."
+                    else
+                        echo "Error: Failed to mount $drive_path to $mount_point."
+                    fi
+                else
+                    echo "Error: Directory $mount_point does not exist."
+                fi
+            else
+                echo "Error: Device $drive_path does not exist."
+            fi
             ;;
         3)
             read -p "Enter the directory to unmount: " mount_point
-            sudo umount $mount_point
-            echo "Drive unmounted successfully."
+            if mountpoint -q $mount_point; then
+                sudo umount $mount_point
+                if [ $? -eq 0 ]; then
+                    echo "Drive unmounted successfully."
+                else
+                    echo "Error: Failed to unmount $mount_point."
+                fi
+            else
+                echo "Error: $mount_point is not a mount point."
+            fi
             ;;
         4)
             echo "Exiting..."
